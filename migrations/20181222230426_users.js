7d542cf77ceaf80511f0448f6e1796f66f05102e
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-12-22 23:08:07 -0700

    "Preliminary Users table added"

---
+++ migrations/20181222230426_users.js
@@ -0,0 +1,17 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('users', function (table) {
+    // TABLE COLUMN DEFINITIONS HERE
+    table.increments()
+    table.string('username', 255).notNullable().defaultTo('')
+    table.string('firstName', 255).notNullable().defaultTo('')
+    table.string('lastName', 255).notNullable().defaultTo('')
+    table.string('password', 255).notNullable().defaultTo('')
+    table.string('email', 255).notNullable().defaultTo('')
+    table.string('desc', 255).defaultTo('')
+    table.string('photoUrl', 255).defaultTo('www.placekitten.com/200/200')
+    table.timestamps(true, true)
+  })
+}
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('users')
+}
