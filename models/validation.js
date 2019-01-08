cbe8af5966da55068b519127b5b9c6b80ebaa4cb
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-08 10:22:29 -0700

    Login working

--- models/validation.js
+++ models/validation.js
@@ -2,10 +2,10 @@ const Joi = require('joi')
 const bcrypt = require('bcrypt')
 
 const schema = Joi.object().keys({
-  username: Joi.string().alphanum().min(3).max(30).required(),
+  username: Joi.string().alphanum().min(8).max(15).required(),
   password: Joi.string().regex(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/).required(),
-  firstName: Joi.string().min(3).max(30).required(),
-  lastName: Joi.string().min(3).max(30).required(),
+  firstName: Joi.string().min(2).max(30).required(),
+  lastName: Joi.string().min(2).max(30).required(),
   email: Joi.string().email({ minDomainAtoms: 2 }).regex(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/).required(),
 })
 
