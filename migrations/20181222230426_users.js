65b34ce5ea1617141765b7eb82f75a4fc361ee41
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-22 07:50:06 -0700

    "Heroku push"

--- migrations/20181222230426_users.js
+++ migrations/20181222230426_users.js
@@ -8,7 +8,7 @@ exports.up = function (knex, Promise) {
     table.string('password', 255).notNullable().defaultTo('')
     table.string('email', 255).notNullable().unique().defaultTo('')
     table.string('desc', 255).defaultTo('')
-    table.string('photoUrl', 255).defaultTo('www.placekitten.com/200/200')
+    table.string('photoUrl', 255).defaultTo('http://www.placekitten.com/200/200')
     table.timestamps(true, true)
   })
 }
