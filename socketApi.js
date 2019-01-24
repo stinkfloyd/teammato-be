9563a82775fdf8721a233f1a540e134615a634e9
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-24 10:53:04 -0700

    "heroku"

--- socketApi.js
+++ socketApi.js
@@ -29,7 +29,8 @@ io.on("connection", (socket) => {
   socket.on('new-message', (message) => {
     console.log("teamName (should work) ", socket.teamName)
     console.log("message ", message)
-    io.to(`${socket.teamName}`).emit('new-message', message)
+    console.log("typeOf: ", typeof teamName)
+    io.to(socket.teamName).emit('new-message', message)
   })
 
   socket.on('disconnect', function () {
