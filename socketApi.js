94cfabc7fa08630863ba5989aca53056782e745c
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-23 19:32:37 -0700

    "heroku"

--- socketApi.js
+++ socketApi.js
@@ -11,9 +11,12 @@ io.on("connection", (socket) => {
 
   socket.on("Team", (team) => {
     socket.leaveAll()
-    console.log("team.name", team.name)
+    console.log("team.name: ", team.name)
+    console.log("socket.id: ", socket.id)
+    console.log("socket.rooms (before): ", socket.rooms)
     teamName = team.name
     socket.join(team.name, () => {
+      console.log("socket.rooms (after): ", socket.rooms)
     })
   })
 
@@ -23,6 +26,7 @@ io.on("connection", (socket) => {
 
   socket.on('new-message', (message) => {
     console.log("teamName ", teamName)
+    console.log("message ", message)
     io.to(teamName).emit('new-message', message)
   })
 })
