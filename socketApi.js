9fe259b3c950360d8265be133828a7fff0194a43
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2019-01-30 18:42:07 -0700

    "Heroku  crash fix"

--- socketApi.js
+++ socketApi.js
@@ -17,11 +17,12 @@ io.on("connection", (socket) => {
     console.log("socket.id: ", socket.id)
     console.log("socket.rooms (before): ", socket.rooms)
     socket.teamName = team.name
+    socket.username = team.username
     socket.join(socket.teamName, () => {
       console.log("socket.rooms (after): ", socket.rooms)
       const logInEmit = {
         user: '**System**',
-        message: `${team.username} has logged in`,
+        message: `${socket.username} has logged in`,
         timestamp: new Date(Date.now())
       }
       io.in(`${socket.teamName}`).emit('new-message', logInEmit)
@@ -85,7 +86,7 @@ io.on("connection", (socket) => {
     console.log('socket.id disconnected:', socket.id)
     const logOutEmit = {
       user: '**System**',
-      message: `${team.username} has logged out`,
+      message: `${socket.username} has logged out`,
       timestamp: new Date(Date.now())
     }
     io.in(`${socket.teamName}`).emit('new-message', logOutEmit)
